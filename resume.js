929ce040caf9c9cb27541b7e3bb0f5a424581649
vivek kumar panneer selvam <145591430+vivekkuma2610@users.noreply.github.com>
2023-09-28 20:02:14 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,62 @@
+let myResume={
+    "personal information": {
+      "name": "Vivek Kumar Panneer Selvam",
+      "email": "vivekpanneer2610@gamil.com",
+      "phone": 9003159788,
+      "Qualification": "M.E",
+      "location": {
+        "address": "10 malayathamman koil street",
+        "postalCode": 600050,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/vivek-kumar-b42ba873/",
+          "github":"https://github.com/vivekkuma2610"
+        }
+      ]
+    },
+    
+    "PG degree": [
+      {
+        "institution": "College Of Engineering Guindy",
+        "department": "computer integrated manufacturing",
+        "year of passing": 2016,
+        "gpa": 7.5,
+      }
+    ],
+
+    "UG degree": [
+        {
+          "institution": "Hindustan institute of technology and science ",
+          "department": "mechanical engineering",
+          "year of passing": 2012,
+          "gpa": 7.9,
+        }
+      ],
+
+    "skills": [
+      {
+        "name": "javascript",
+        "level": "beginer",
+        "project": [
+          "automatic attendance maintaing system using python language"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "script writter,youtuber,",
+      }
+    ]
+  }
+  console.log(myResume);
+  
